2df75df757330c1244cdb7c7e721990e076ff273
Jérôme Nadaud <jerome.nadaud@prestashop.com>
2015-05-12 08:45:22 +0200

    // Version updated to v1.5.5

--- blockcmsinfo.php
+++ blockcmsinfo.php
@@ -38,7 +38,7 @@ class Blockcmsinfo extends Module
 	{
 		$this->name = 'blockcmsinfo';
 		$this->tab = 'front_office_features';
-		$this->version = '1.5.4';
+		$this->version = '1.5.5';
 		$this->author = 'PrestaShop';
 		$this->bootstrap = true;
 		$this->need_instance = 0;
