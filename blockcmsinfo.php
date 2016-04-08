1cdc3597b4780ef8c5b624e1f24a46a0bcf467fb
Julien Bourdeau <julien@sigerr.org>
2016-04-08 16:38:59 +0200

    // not compatible with PrestaShop 1.7

--- blockcmsinfo.php
+++ blockcmsinfo.php
@@ -47,7 +47,7 @@ class Blockcmsinfo extends Module
 
 		$this->displayName = $this->l('Custom CMS information block');
 		$this->description = $this->l('Adds custom information blocks in your store.');
-		$this->ps_versions_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_);
+		$this->ps_versions_compliancy = array('min' => '1.6', 'max' => '1.6.99.99');
 	}
 
 	public function install()
